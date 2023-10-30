966f3b7eb494378fbe2c8deb78e69ac4c7195553
OscarT99 <oatorres004@misena.edu.co>
2023-10-30 15:49:46 -0500

    primer commit

---
+++ src/routes/routesContructor.js
@@ -0,0 +1,7 @@
+// const clienteRoutes = require('./clienteRoute/clienteRoute');
+
+function configureRoutes(app, path) {
+    // app.use(path, clienteRoutes);
+    }
+
+module.exports = configureRoutes;
