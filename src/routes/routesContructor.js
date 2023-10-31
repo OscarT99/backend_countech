c4430132611a164a2cce49c7a09e6a2941d4fa3f
OscarT99 <oatorres004@misena.edu.co>
2023-10-30 19:59:16 -0500

    primer commit pedido

--- src/routes/routesContructor.js
+++ src/routes/routesContructor.js
@@ -1,7 +1,17 @@
 const clienteRoutes = require('./clienteRoute/clienteRoute');
+const pedidoRoutes = require('./pedidoRoutes/pedidoRoute')
+const referenciaPedidoRoutes = require('./pedidoRoutes/referenciaPedidoRoute')
+const procesoReferenciaPedidoRoutes = require('./pedidoRoutes/procesoReferenciaPedidoController')
+const colorProcesoReferenciaPedidoRoutes = require('./pedidoRoutes/colorProcesoReferenciaPedidoRoute')
+const tallaColorProcesoReferenciaPedidoRoutes = require('./pedidoRoutes/tallaColorProcesoReferenciaPedidoController')
 
 function configureRoutes(app, path) {
     app.use(path, clienteRoutes);
+    app.use(path, pedidoRoutes);
+    app.use(path, referenciaPedidoRoutes);
+    app.use(path, procesoReferenciaPedidoRoutes);
+    app.use(path, colorProcesoReferenciaPedidoRoutes);
+    app.use(path, tallaColorProcesoReferenciaPedidoRoutes);
     }
 
 module.exports = configureRoutes;
